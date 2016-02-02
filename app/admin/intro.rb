c89504966cb8cd0366705be57d49dc72b48d496a
Kiko <mmhabib11@gmail.com>
2016-02-02 05:25:42 -0500

    remove fields from active admin

--- app/admin/intro.rb
+++ app/admin/intro.rb
@@ -3,4 +3,15 @@ ActiveAdmin.register Intro do
 menu parent: "Sections"
 permit_params :title, :description, :image, :scroll
 
+index do
+  column :title
+  column :description
+  column :image
+ end
+
+form do |f|
+      f.inputs :title, :description, :image, :scroll
+      f.actions
+end
+
 end
