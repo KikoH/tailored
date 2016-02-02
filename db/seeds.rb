533303a574949ae55e1933005cf51544c0015cc1
Kiko <mmhabib11@gmail.com>
2016-02-02 05:03:24 -0500

    change email of admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'kiko@tailored.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'nermin@tailored.com', password: 'password', password_confirmation: 'password')
