90295a01937543c2409540c3390170c2384130a3
Kiko <mmhabib11@gmail.com>
2016-02-02 17:19:30 -0500

    Remove bootstrap sprockets and Add maps logic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require jquery.min
 //= require jquery-migrate.min
 //= require moderniz.min
--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,16 +1,25 @@
-$(document).ready(function() {
-    var map;
-    function initMap() {
-      var myLatLng = {lat: -34.397, lng: 150.644};
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
-        zoom: 15
-    });
-
-      var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Tailored'
-    });
-  }
+$(window).load(function() {
+  loadScript();
 });
+
+var map;
+function initMap() {
+  var myLatLng = {lat: -34.397, lng: 150.644};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 15
+});
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Tailored'
+});
+}
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBKtTdsu4z83PUNmiKhAb0tPKTBoqY7lSw&callback=initMap"
+  document.body.appendChild(script);
+}
