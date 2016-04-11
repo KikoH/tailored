a589146787193118663562ca48227d59b07d831b
Kiko <mmhabib11@gmail.com>
2016-04-11 16:59:18 -0400

    Active admin paragraph form for team

--- app/admin/team.rb
+++ app/admin/team.rb
@@ -1,7 +1,11 @@
 ActiveAdmin.register Team do
 
-menu parent: "Sections"
-permit_params :title, :subtitle, :image, :name, :position, :email, :description
+	menu parent: "Sections"
+	permit_params :title, :subtitle, :image, :name, :position, :email, :description
 
-
+	form do |f|
+		f.inputs :title, :subtitle, :image, :name, :position, :email
+		f.input :description, :input_html => {:rows => 5, :cols => 50},  as: :text
+		f.actions
+	end
 end
