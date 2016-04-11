ed29b2ad4304bb7547bc1df808876dd3b4c075fc
Kiko <mmhabib11@gmail.com>
2016-04-11 19:12:38 -0400

    Add more fields to email and set up controller

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,12 @@
 class ContactMailer < ActionMailer::Base
 	#Create a method that assigns the params to instace variables
-	def new_feedback(name, email, message)
+	def new_feedback(name, email, hear, can_publish, held, message)
 		@name = name
 		@email = email
 		@message = message
+		@hear = hear
+		@can_publish = can_publish
+		@held = held
 		# Mail it to/from/subject
 		mail(to: 'kiiko87@gmail.com', from: @email, subject: 'Feedback')
 	end
