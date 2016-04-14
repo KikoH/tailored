f41d421636650578cf5de6a1ddb9a75281545f25
Kiko <mmhabib11@gmail.com>
2016-04-14 01:35:51 -0400

    add portfolio id to portfolios

--- db/migrate/20160220132531_create_images.rb
+++ db/migrate/20160220132531_create_images.rb
@@ -1,7 +1,7 @@
 class CreateImages < ActiveRecord::Migration
 	def change
 		create_table :images do |t|
-			t.integer :image_id
+			t.integer :portfolio_id
 			t.string :image
 			t.timestamps null: false
 		end
