f56f06fda817ac592b45a9f3530dd9416e8ec65e
Kiko <mmhabib11@gmail.com>
2016-04-21 15:47:06 -0400

    Active admin team navigation changes

--- app/admin/team.rb
+++ app/admin/team.rb
@@ -3,6 +3,15 @@ ActiveAdmin.register Team do
 	menu parent: "Sections"
 	permit_params :title, :subtitle, :image, :name, :position, :email, :description
 
+	index do
+		selectable_column
+		column "Title" do |team|
+			link_to team.title, admin_team_path(team)
+		end
+		column :name
+		column :position
+	end
+
 	form do |f|
 		f.inputs :title, :subtitle, :image, :name, :position, :email
 		f.input :description, :input_html => {:rows => 5, :cols => 50},  as: :text
